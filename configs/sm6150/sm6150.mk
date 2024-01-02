91a0bdb467f71a919a67bceff3b53348e7a41bba
electimon <electimon@gmail.com>
2024-01-02 18:02:39 +0000

    sm6150: Don't copy config xml
    Signed-off-by: electimon <electimon@gmail.com>

--- configs/sm6150/sm6150.mk
+++ configs/sm6150/sm6150.mk
@@ -158,14 +158,6 @@ PRODUCT_PACKAGES += $(AUDIO_DLKM)
 DEVICE_PACKAGE_OVERLAYS += vendor/qcom/opensource/audio/sm8150/configs/common/overlay
 
 #XML Audio configuration files
-ifeq ($(GENERIC_ODM_IMAGE),true)
-PRODUCT_COPY_FILES += \
-    $(TOPDIR)vendor/qcom/opensource/audio/sm8150/configs/sm6150/audio_policy_configuration_odm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
-else
-PRODUCT_COPY_FILES += \
-    $(TOPDIR)vendor/qcom/opensource/audio/sm8150/configs/common/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
-endif
-
 PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
